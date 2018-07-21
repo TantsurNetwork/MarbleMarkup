9c2f6c15ec7aae844b2e038e83507993ec9ad6c8
Максим <tantsurm@gmail.com>
2018-07-21 13:10:51 +0300

    working one

--- server.js
+++ server.js
@@ -16,59 +16,59 @@ const server = http.createServer( (request, response) => {
 			break;
 		// sample
 		case '/img/icon1.png':
-			response.end( fs.readFileSync("img/icon1.png", "utf8") );
+			response.end( fs.readFileSync("img/icon1.png") );
 			break;
 
 		case '/img/icon2.png':
-			response.end( fs.readFileSync('img/icon2.png', 'utf8') );
+			response.end( fs.readFileSync('img/icon2.png') );
 			break;
 
 		case '/img/icon3.png':
-			response.end( fs.readFileSync('img/icon3.png', 'utf8') );
+			response.end( fs.readFileSync('img/icon3.png') );
 			break;
 
 		case '/img/icon4.png':
-			response.end( fs.readFileSync('img/icon4.png', 'utf8') );
+			response.end( fs.readFileSync('img/icon4.png') );
 			break;
 
 		case '/img/snap1.png':
-			response.end( fs.readFileSync('img/snap1.png', 'utf8') );
+			response.end( fs.readFileSync('img/snap1.png') );
 			break;
 
 		case '/img/snap2.png':
-			response.end( fs.readFileSync('img/snap2.png', 'utf8') );
+			response.end( fs.readFileSync('img/snap2.png') );
 			break;
 
 		case '/img/snap3.png':
-			response.end( fs.readFileSync('img/snap3.png', 'utf8') );
+			response.end( fs.readFileSync('img/snap3.png') );
 			break;
 
 		case '/img/logo.png':
-			response.end( fs.readFileSync('img/logo.png', 'utf8') );
+			response.end( fs.readFileSync('img/logo.png') );
 			break;
 
 		case '/img/banner.png':
-			response.end( fs.readFileSync('img/banner.png', 'utf8') );
+			response.end( fs.readFileSync('img/banner.png') );
 			break;
 
 		case '/img/fill.png':
-			response.end( fs.readFileSync('img/fill.png', 'utf8') );
+			response.end( fs.readFileSync('img/fill.png') );
 			break;
 
 		case '/img/phone.png':
-			response.end( fs.readFileSync('img/phone.png', 'utf8') );
+			response.end( fs.readFileSync('img/phone.png') );
 			break;
 
 		case '/img/message.png':
-			response.end( fs.readFileSync('img/message.png', 'utf8') );
+			response.end( fs.readFileSync('img/message.png') );
 			break;
 
 		case '/img/tweet.png':
-			response.end( fs.readFileSync('img/tweet.png', 'utf8') );
+			response.end( fs.readFileSync('img/tweet.png') );
 			break;
 
 		case '/img/quote.png':
-			response.end( fs.readFileSync('img/quote.png', 'utf8') );
+			response.end( fs.readFileSync('img/quote.png') );
 			break;
 
 
